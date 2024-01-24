956a5179d1828f34fc837b670b3c006b6e6ae395
cViviana <leidyv@unicauca.edu.co>
2024-01-23 22:45:39 -0500

    Refactoring the method userCanLoginViaTheHomePage

--- src/test/java/starter/sauceDemo/WhenLoggingIn.java
+++ src/test/java/starter/sauceDemo/WhenLoggingIn.java
@@ -7,24 +7,25 @@ import org.junit.jupiter.api.extension.ExtendWith;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import net.serenitybdd.annotations.Managed;
+import net.serenitybdd.core.steps.UIInteractions;
 import net.serenitybdd.junit5.SerenityJUnit5Extension;
 
 
 
 @ExtendWith(SerenityJUnit5Extension.class)
-public class WhenLoggingIn {
+public class WhenLoggingIn extends UIInteractions {
     
     @Managed
     WebDriver driver;
 
     @Test
     public void userCanLoginViaTheHomePage() {
-        driver.get("https://saucedemo.com");
+        openUrl("https://saucedemo.com");
 
-        driver.findElement(By.cssSelector("[data-test='username']")).sendKeys("standard_user");
-        driver.findElement(By.cssSelector("[data-test='password']")).sendKeys("secret_sauce");
-        driver.findElement(By.cssSelector("[data-test='login-button']")).click();
+        find("[data-test='username']").sendKeys("standard_user");
+        find("[data-test='password']").sendKeys("secret_sauce");
+        find("[data-test='login-button']") .click();
 
-        assertEquals(driver.findElement(By.cssSelector(".title")).getText(),"Products");
+        assertEquals(find(".title").getText(),"Products");
     }
 }
